2041983f3750a22f35b0e8f28efa07c92eb77582
DLX <denver@ii.org.nz>
2016-09-09 23:21:56 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -9,5 +9,5 @@ bundle exec rake db:setup
 bundle exec rake jobs:work &
 bundle exec rspec &
 
-RACK_ENV=test SMTP_SERVER=smtp.gmail.com SMTP_PORT=587 SMTP_USERNAME=smtp@hippiehacker.org SMTP_PASSWORD=73%b%8AX SMTP_DOMAIN=gmail.com bundle exec rails s
+RACK_ENV=production SMTP_SERVER=smtp.gmail.com SMTP_PORT=587 SMTP_USERNAME=smtp@hippiehacker.org SMTP_PASSWORD=73%b%8AX SMTP_DOMAIN=gmail.com bundle exec rails s
 
