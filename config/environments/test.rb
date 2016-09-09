821ff3d4ae854eed1a0d477ec7e675ddc71ab9ea
DLX <denver@ii.org.nz>
2016-09-09 23:08:28 +0200

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,16 +29,9 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.smtp_settings = {
-    :address        => ENV['SMTP_SERVER'],
-    :port           => ENV['SMTP_PORT'],
-    :authentication => :plain,
-    :user_name      => ENV['SMTP_USERNAME'],
-    :password       => ENV['SMTP_PASSWORD'],
-    :domain         => ENV['SMTP_DOMAIN'],
-    :enable_starttls_auto => true
-  }
 
   # commented out because it is overriding application.rb active_job.queue_adapter = :delayed_job
   # config.active_job.queue_adapter = :test
