a80bf0dfeea71f57ab3ffb754e39136a5f2e831c
DLX <denver@ii.org.nz>
2016-09-09 23:14:19 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DEV_DB_NAME=cobudget_development
 export TEST_DB_NAME=cobudget_test
+export PRODUCTION_DB_NAME=cobudget_production
 export DB_USERNAME=postgres
 export DB_PASSWORD=
 
