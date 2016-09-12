ec99ee225402c837432a9c100211b604223d7d49
DLX <denver@ii.org.nz>
2016-09-13 00:18:01 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RACK_ENV=test RAILS_ENV=test bundle exec rake db:exists && rake db:migrate || rake db:setup
-RACK_ENV=test RAILS_ENV=test bundle exec rake jobs:work &
-RACK_ENV=test RAILS_ENV=test bundle exec rails s
+bundle exec rake db:exists && rake db:migrate || rake db:setup
+bundle exec rake jobs:work &
+bundle exec rails s
